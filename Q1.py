6d444983111c1e0b7c7b7405784ee21d38bfe362
dtonda8 <davidbatonda7@gmail.com>
2024-08-20 20:17:21 +1000

    Add sols

--- Q1.py
+++ Q1.py
@@ -1,9 +1,18 @@
-from typing import List, TypeVar
+from typing import TypeVar
 from data_structures.linked_list import LinkedList
 T = TypeVar("T")
 
 def remove_second_half(ll: LinkedList[T]) -> LinkedList[T]:
-	raise NotImplementedError()
+    n = len(ll)
+    goal_length = n // 2
+    
+    # If odd, increment goal length by 1
+    if n % 2 == 1:
+        goal_length += 1
+        
+    while len(ll) > goal_length:
+        ll.delete_at_index(goal_length)
+    return ll
 
 if __name__ == "__main__": # for personal tests
 	pass
--- Q2.py
+++ Q2.py
@@ -1,7 +1,20 @@
+from data_structures.list_adt import List
 from data_structures.referential_array import ArrayR
+from data_structures.array_sorted_list import ArraySortedList
 
 def kth_smallest(nums: ArrayR, queries: ArrayR) -> ArrayR:
-	raise NotImplementedError()
+    # add to sorted list
+    sorted_lst = ArraySortedList(len(nums))
+    for i in range(len(nums)):
+        sorted_lst.add(nums[i])
+        
+    # answer to queries
+    results = ArrayR(len(queries))
+    for i in range(len(queries)):
+        idx = queries[i] - 1
+        results[i] = sorted_lst[idx]
+    
+    return results
 
 if __name__ == "__main__": # for personal tests
 	pass
